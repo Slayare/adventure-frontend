b9481b6e4606d88c1ceee7d7d03cdace35f24fef
Dan <officialslayare@gmail.com>
2024-05-03 13:56:34 +1000

    Create base list component

---
+++ src/base/List/List.test.tsx
@@ -0,0 +1,16 @@
+// List.test.tsx
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import List from "./List";
+
+describe("List", () => {
+  it("should render a list with items", () => {
+    const items = ["1", "2", "3"];
+    render(<List items={items} />);
+
+    items.forEach((item) => {
+      expect(screen.getByText(item)).toBeInTheDocument();
+    });
+  });
+});
---
+++ src/base/List/List.tsx
@@ -0,0 +1,35 @@
+import {
+  styled,
+  List as MuiList,
+  ListProps as MuiListProps,
+  ListItem,
+} from "@mui/joy";
+import React from "react";
+
+interface ListProps extends MuiListProps {
+  items: any[];
+}
+
+const StyledList = styled(MuiList)(({ theme }) => ({
+  [theme.breakpoints.down("mobile")]: {
+    // do something
+  },
+  [theme.breakpoints.between("mobile", "tablet")]: {
+    // do something
+  },
+  [theme.breakpoints.up("tablet")]: {
+    // do something
+  },
+}));
+
+const List = ({ items, ...props }: ListProps) => {
+  return (
+    <StyledList {...props}>
+      {items.map((item, index) => (
+        <ListItem key={index}>{item}</ListItem>
+      ))}
+    </StyledList>
+  );
+};
+
+export default List;
