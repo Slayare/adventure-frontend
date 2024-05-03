73d2ad73a631fba81a172926267c5ff2e38a604f
Dan <officialslayare@gmail.com>
2024-05-03 20:14:29 +1000

    Add config setup for gifs

---
+++ types.d.ts
@@ -0,0 +1,9 @@
+declare module "*.webp" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.gif" {
+  const value: string;
+  export = value;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,10 @@ module.exports = {
         ],
         exclude: /node_modules/,
       },
+      {
+        test: /\.(png|jpe?g|gif|webp)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
