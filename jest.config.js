30e495b51b6deb85e7e7243cf4aa76b2db4655f9
Dan <officialslayare@gmail.com>
2024-05-18 13:48:04 +1000

    Remove unneeded module mapper for zustand

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
     "^@/(.*)$": "<rootDir>/src/$1",
     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|avif)$":
       "<rootDir>/__mocks__/fileMock.js",
-    "^zustand$": "<rootDir>/__mocks__/zustand.ts",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   transform: {
