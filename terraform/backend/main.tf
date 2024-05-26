f15a24733cc522bd4a0c46362fc26f712beef50e
Dan <officialslayare@gmail.com>
2024-05-26 19:14:47 +1000

    Add timeout

--- terraform/backend/main.tf
+++ terraform/backend/main.tf
@@ -48,4 +48,8 @@ resource "aws_dynamodb_table_export" "export_dynamo_to_s3" {
   depends_on = [data.aws_dynamodb_table.terraform_locks]
   table_arn = data.aws_dynamodb_table.terraform_locks.arn
   s3_bucket = data.aws_s3_bucket.tf_state.bucket
+
+  timeouts {
+    create = "20m"
+  }
 }
