763c9ca8b4330f14a58bb951a256badf32b94748
Dan <officialslayare@gmail.com>
2024-05-30 21:09:58 +1000

    Remove backend

--- terraform/backend/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-  description = "User access key"
-  type        = string
-}
-
-variable "secret_access_key" {
-  description = "Secret access key"
-  type        = string
-}
